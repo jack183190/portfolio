23c1ddd43065068f9addb7505becfb58d01df499
jack183190 <119117553+jack183190@users.noreply.github.com>
2026-04-28 12:23:19 +0100

    Update performance metrics and optimization details

--- projects/spire.js
+++ projects/spire.js
@@ -13,18 +13,19 @@ spire.itch = "";
 spire.discord = "";
 spire.libs = "C++, Vulkan, vulkan-memory-allocator, glslang, ImGui";
 spire.longDesc = "Over the last year I have implemented a variety of optimisations such as indirect drawing, binary greeding meshing, and vertex compression. "
-  + "Currently Spire can render the same 32x32 Minecraft chunks about 3x faster than Minecraft, however Spire lags behind in terms of memory usage. In addition," +
-  " Minecraft is much more complciated than Spire and has transparent voxels, vfx, particles, entities, so the comparison is unfair."
+  + "Currently Spire can render the same 512x384x512 voxel area about 5x faster than Minecraft, with 7x less VRAM."
   + "<br><br>The world is split into chunks of 64x64x64 voxels. A mesh is generated from each chunk, this has to happen whenever a voxel is "
   + " modified in the chunk or on the border of a neigbhouring chunk. All the chunks are drawn at once using an indirect draw call.<br><br>"
   + "Only the surface faces of voxels need to be rendered, this is done by checking if the voxel adjacent to a face is present, if it is "
   + "then don't add the face to the vertex buffer.<br><br>"
-  + "Greedy meshing is used to further optimise generated meshes, it merges adjacent faces into a single face, reducing the number of vertices "
+  + "Binary greedy meshing is used to further optimise generated meshes, it merges adjacent faces into a single face, reducing the number of vertices "
   + "that need to be stored. The drawback of this approach is that voxel types can no longer be stored in the vertex. I've solved this by "
-  + "building a hash map when I generate the mesh mapping each voxel position to its voxel type and sending it to the GPU. "
-  + "The voxel position can be calculated by flooring the fragment position." 
+  + "building a custom data structure when I generate the mesh mapping each quad to a 2D array of all voxel data contained within the quad. "
   + "<br><br>For more information about the Vulkan abstractions, see: <a href=\"https://github.com/underscore95/Spire/blob/main/Documentation/SPIRE.md\">SPIRE.md</a>"
   + "<br>For more information about the voxel rendering, see: <a href=\"https://github.com/underscore95/Spire/blob/main/Documentation/VOXELS.md\">VOXELS.md</a>"
+  + "<br>I also created a YouTube video explaining my greedy meshing implementation along with my custom data structure for voxel data storage:"
+  + "<iframe width=\"560\" height=\"315\" src=\"https://www.youtube.com/embed/Z3atq5uAmOY?si=eNfqEHgIlm3Bl8uX\" title=\"YouTube video player\" frameborder=\"0\" allow=\"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share\" "
+  + "referrerpolicy=\"strict-origin-when-cross-origin\" allowfullscreen></iframe>";
 spire.homepageProject = true;
 spire.url = "spire.html";
 
